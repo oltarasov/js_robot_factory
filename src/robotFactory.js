6247639570a97be9ef8159b96fb93093bb0fbf70
Oleg Tarasov <tarasov.oleg.9779@gmail.com>
2023-08-21 14:42:23 +0300

    robot factory v1

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,92 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
 
-class FlyingRobot {}
+    if (!coords) {
+      this.coords = {
+        x: 0, y: 0,
+      };
+    } else {
+      this.coords = {
+        x: coords.x !== undefined ? coords.x : 0,
+        y: coords.y !== undefined ? coords.y : 0,
+      };
+    }
 
-class DeliveryDrone {}
+    this.chipVersion = chipVersion;
+  };
+
+  goForward(step = 1) {
+    this.coords.y += step;
+
+    return this;
+  };
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+
+    return this;
+  };
+
+  goRight(step = 1) {
+    this.coords.x += step;
+
+    return this;
+  };
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+
+    return this;
+  };
+
+  getInfo() {
+    return `Robot: ${this.name},`
+    + ` Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords.z = coords && coords.z !== undefined ? coords.z : 0;
+  };
+
+  goUp(step = 1) {
+    this.coords.z += step;
+
+    return this;
+  };
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+
+    return this;
+  };
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad || null;
+  };
+
+  hookLoad(newCurrentLoad) {
+    if (this.currentLoad === null) {
+      if (this.maxLoadWeight >= newCurrentLoad.weight) {
+        this.currentLoad = newCurrentLoad;
+      }
+    };
+  };
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
